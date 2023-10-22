46910a18310b177375b3f09c9ad294f6ec894857
Adam D. Ruppe <destructionator@gmail.com>
2023-10-22 13:27:46 -0400

    better $ escape

--- 02-formatting.d
+++ 02-formatting.d
@@ -6,8 +6,10 @@ void main() {
 	string name = "D user";
 	float wealth = 55.70;
 
-	// double dollar sign = literal $ in the output
+	// only $ followed by a D identifier or a ( is special
+	// so the double $ here is a basic one followed by an
+	// interpolated var. Can also use \$ in i"strings"
 	// then :% is interpreted by this function to mean
 	// "use this format string for the preceding argument"
-	writefln(i"$name has $$$wealth:%0.2f");
+	writefln(i"$name has $$wealth:%0.2f");
 }
